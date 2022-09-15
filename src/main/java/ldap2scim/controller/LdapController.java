61ff9c2f2be84bf28fe18a6dd7871fd86947a340
程超 <charles.chengc@alibaba-inc.com>
2022-09-15 14:18:54 +0800

    add sync task

--- src/main/java/ldap2scim/controller/LdapController.java
+++ src/main/java/ldap2scim/controller/LdapController.java
@@ -1,5 +1,6 @@
 package ldap2scim.controller;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,13 +9,17 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.support.CronExpression;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ldap2scim.common.CommonConstants;
 import ldap2scim.model.WebResult;
 import ldap2scim.service.LdapService;
+import ldap2scim.task.Ldap2ScimTask;
 import ldap2scim.utils.JsonUtils;
 
 /**
@@ -88,6 +93,68 @@ public class LdapController extends BaseController {
                 }
             }
             LdapService.syncLdaptoScim(targetList);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            result.setSuccess(false);
+            result.setErrorMsg(e.getMessage());
+        }
+        outputToJSON(response, result);
+    }
+
+    @RequestMapping("/getSearchParams.do")
+    public void getSearchParams(HttpServletRequest request, HttpServletResponse response) {
+        WebResult result = new WebResult();
+        try {
+            Map<String, String> map = new HashMap<>();
+            map.put("ldapbase", CommonConstants.LDAP_Searchbase);
+            map.put("ldapfilter", CommonConstants.LDAP_Searchfilter);
+            result.setData(map);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            result.setSuccess(false);
+            result.setErrorMsg(e.getMessage());
+        }
+        outputToJSON(response, result);
+    }
+
+    @RequestMapping("/getTaskInfo.do")
+    public void getTaskInfo(HttpServletRequest request, HttpServletResponse response) {
+        WebResult result = new WebResult();
+        try {
+            StringBuilder infoDiv = new StringBuilder();
+            infoDiv.append("定时任务启用:" + CommonConstants.SCIM_SYNC_CRON_ENABLED);
+            infoDiv.append("<br/>");
+            infoDiv.append("定时任务表达式:" + CommonConstants.SCIM_SYNC_CRON_EXPRESSION);
+            infoDiv.append("<br/>");
+            infoDiv.append("LDAP Searchbase:" + CommonConstants.LDAP_Searchbase);
+            infoDiv.append("<br/>");
+            infoDiv.append("LDAP Searchfilter:" + CommonConstants.LDAP_Searchfilter);
+            infoDiv.append("<br/>");
+            infoDiv.append("<br/>");
+
+            if (CommonConstants.SCIM_SYNC_CRON_ENABLED
+                && StringUtils.isNotBlank(CommonConstants.SCIM_SYNC_CRON_EXPRESSION)) {
+                CronExpression cronExpression = CronExpression.parse(CommonConstants.SCIM_SYNC_CRON_EXPRESSION);
+                infoDiv.append(
+                    "下次运行时间:" + cronExpression.next(LocalDateTime.now()).format(CommonConstants.DateTimeformatter));
+                infoDiv.append("<br/>");
+            }
+
+            result.setData(infoDiv.toString());
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            result.setSuccess(false);
+            result.setErrorMsg(e.getMessage());
+        }
+        outputToJSON(response, result);
+    }
+
+    @RequestMapping("/getTaskRecords.do")
+    public void getTaskRecords(HttpServletRequest request, HttpServletResponse response) {
+        WebResult result = new WebResult();
+        try {
+            result.setTotal(Ldap2ScimTask.taskRecords.size());
+            result.setData(Ldap2ScimTask.taskRecords);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
             result.setSuccess(false);
